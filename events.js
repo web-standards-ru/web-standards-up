c9071c268af7604f8709e28dc7998cf38c50af14
Mikhail Baranov <mikhail.baranov@gmail.com>
2015-10-09 12:25:43 +0300

    Добавил первый день Fronteers

--- events.js
+++ events.js
@@ -1,5 +1,12 @@
 export default [
     {
+        date: '2015-10-08',
+        shortName: 'fronteers',
+        fullName: 'Fronteers',
+        location: 'Амстердам, Голландия',
+        firstTweetId: '652058872258142208'
+    },
+    {
         date: '2015-08-29',
         shortName: 'frontend-union-conf',
         fullName: 'Frontend Union Conf',
