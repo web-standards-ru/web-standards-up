7b170502fc353f2f02f6527af68b736799a19a5a
Mikhail Baranov <mikhail.baranov@gmail.com>
2015-11-12 00:53:32 +0300

    Fix newlines in commit messages
    [ci skip]

--- publish.sh
+++ publish.sh
@@ -17,10 +17,10 @@ echo "save dump"
 git fetch --depth=5 https://wsd-bot:${GITHUB_TOKEN}@github.com/web-standards-ru/web-standards-up.git master:refs/remotes/origin/master
 git checkout master
 git add dump
-git commit -m "save dump\n\n[ci skip]"
+git commit -m $'Save dump\n\n[ci skip]'
 
 echo "save event data"
 git add .
 ADDED_MD=`git status -s | sed 's/^A  //g'`
-git commit -m "save ${ADDED_MD}\n\n[ci skip]"
+git commit -m 'Save '${ADDED_MD}$'\n\n[ci skip]'
 git push origin master &>/dev/null
