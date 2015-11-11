6072b63eae8bcead4db59c093a69ddf7f4cd8540
Mikhail Baranov <mikhail.baranov@gmail.com>
2015-11-12 00:23:22 +0300

    Fix stupid bug

--- publish.sh
+++ publish.sh
@@ -1,27 +1,25 @@
 #!/bin/bash
 
 echo "set git environment"
-echo ${GITHUB_EMAIL}
-echo ${GITHUB_NAME}
 git config user.email ${GITHUB_EMAIL}
 git config user.name ${GITHUB_NAME}
 git remote rm origin
 git remote add origin https://wsd-bot:${GITHUB_TOKEN}@github.com/web-standards-ru/web-standards-up.git
 git checkout engine
 
-#echo "run update"
-#npm run update
-#
-#echo "generate md"
-#npm run generate
-#
-#echo "save dump"
-#git checkout master
-#git add dump
-#git commit -m "save dump\n\n[ci skip]"
-#
-#echo "save event data"
-#git add .
-#ADDED_MD=`git status -s | sed 's/^A  //g'`
-#git commit -m "save ${ADDED_MD}\n\n[ci skip]"
-#git push origin master &>/dev/null
+echo "run update"
+npm run update
+
+echo "generate md"
+npm run generate
+
+echo "save dump"
+git checkout master
+git add dump
+git commit -m "save dump\n\n[ci skip]"
+
+echo "save event data"
+git add .
+ADDED_MD=`git status -s | sed 's/^A  //g'`
+git commit -m "save ${ADDED_MD}\n\n[ci skip]"
+git push origin master &>/dev/null
