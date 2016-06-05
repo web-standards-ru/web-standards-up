edd1ecb9198b4550669cacad42b1d3a0f38898f3
Mikhail Baranov <mikhail.baranov@gmail.com>
2016-06-05 15:54:50 +0300

    Окончательно добавляем второй день ScotlandJS

--- events.js
+++ events.js
@@ -1,12 +1,12 @@
 export default [
-/*    {
+    {
         date: '2016-06-03',
         shortName: 'scotlandjs',
         fullName: 'ScotlandJS',
         location: 'Эдинбург, Шотландия, Великобритания',
         firstTweetId: '738641542714404864',
         lastTweetId: '738780207549718529'
-    }, */
+    },
     {
         date: '2016-06-02',
         shortName: 'scotlandjs',
