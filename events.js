114d217608b8d6dba5a525ed5ca1e0db7146a733
Mikhail Baranov <mikhail.baranov@gmail.com>
2016-06-05 15:18:59 +0300

    Добвил ScotlandCSS и ScotlandJS

--- events.js
+++ events.js
@@ -1,5 +1,29 @@
 export default [
     {
+        date: '2016-06-02',
+        shortName: 'scotlandjs',
+        fullName: 'ScotlandJS',
+        location: 'Эдинбург, Шотландия, Великобритания',
+        firstTweetId: '738641542714404864',
+        lastTweetId: '738780207549718529'
+    },
+    {
+        date: '2016-06-02',
+        shortName: 'scotlandjs',
+        fullName: 'ScotlandJS',
+        location: 'Эдинбург, Шотландия, Великобритания',
+        firstTweetId: '738280930331250688',
+        lastTweetId: '738404835624620032'
+    },
+    {
+        date: '2016-06-01',
+        shortName: 'scotlandcss',
+        fullName: 'ScotlandCSS',
+        location: 'Эдинбург, Шотландия, Великобритания',
+        firstTweetId: '737919204200943616',
+        lastTweetId: '738042558115393536'
+    },
+    {
         date: '2016-05-11',
         shortName: 'cssconfbudapest',
         fullName: 'CSSConf Budapest 2016',
